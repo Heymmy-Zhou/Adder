c4ffbfe00a91a520a878a91ebd7a5739091ca154
黑米粥工作室 <hei.mi.zhou@qq.com>
2020-07-20 10:37:46 +0800

    Add files via upload

---
+++ resource.h
@@ -0,0 +1,26 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 Adder.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_ADDER_DIALOG                102
+#define IDR_MAINFRAME                   128
+#define IDC_ADD1                        1000
+#define IDC_ADD2                        1001
+#define IDC_R                           1002
+#define IDC_RESET                       1003
+#define IDC_ABOUT                       1004
+#define IDC_LICENSE                     1005
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
